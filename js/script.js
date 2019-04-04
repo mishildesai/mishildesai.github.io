351f543aa29602dd60089bdb2bd5d3aeb7e63b24
mishildesai <mishildesai@gmail.com>
2019-04-04 15:16:39 -0400

    Added three new quotes.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
+var quotes = [
+    ["Bill Gates","Life is not fair; get used to it."],
+    ["Richard Stallman","Anything that prevents you from being friendly, a good neighbour, is a terror tactic."],
+    ["Doug Linder","A good programmer is someone who always looks both ways before crossing a one-way street."],
+    ["Oscar Godson","One of the best programming skills you can have is knowing when to walk away for awhile."],
+    ["Michael Hartung","Hardware eventually fails. Software eventually works."],
+    ["Kyle Simpson","There's nothing more permanent than a temporary hack."]
+];
+
 function setQuote() {
-    var quotes = [
-        ["Bill Gates","Life is not fair; get used to it."],
-        ["Richard Stallman","Anything that prevents you from being friendly, a good neighbour, is a terror tactic."],
-        ["Doug Linder","A good programmer is someone who always looks both ways before crossing a one-way street."]
-    ];
     var random = getRndInteger(0, quotes.length - 1);
     document.getElementById("quote").innerHTML = quotes[random][1];
     document.getElementById("author").innerHTML = "-" + quotes[random][0];
